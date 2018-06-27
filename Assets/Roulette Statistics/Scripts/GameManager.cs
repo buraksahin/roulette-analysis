9a1e624811275c7641a2d75cf7ced23acd9328fd
buraksahin <buraksahin.tr@gmail.com>
2018-06-27 05:59:53 +0300

    Add probability text control for number buttons

--- Assets/Roulette Statistics/Scripts/GameManager.cs
+++ Assets/Roulette Statistics/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour{
     void Start(){
         myRoulette = new Roulette();
         myRoulette.Start();
+		HandleUI();
     }
 
 	// Update is called once per frame
@@ -30,6 +31,17 @@ public class GameManager : MonoBehaviour{
 	public void HandleUI(){
 		for (int i = 0; i < 37; i++){
 			numberButtons.GetChild(i).GetChild(2).GetComponent<Text>().text = myRoulette.numbers[i].getRank() + "";
+		}
+		for (int i = 0; i < 37; i++){
+			float tempProb = myRoulette.numbers[i].getProbability();
+			numberButtons.GetChild(i).GetChild(1).GetComponent<Text>().text = tempProb + "";
+
+			if (tempProb >= 0.2){
+				numberButtons.GetChild (i).GetChild (3).GetComponent<Image>().enabled = true;
+			}
+			else {
+				numberButtons.GetChild (i).GetChild (3).GetComponent<Image>().enabled = false;
+			}
 		}
 	}
 }
